21ac54354eef68d78d28def7ad74e48c7126bf7c
Vinh-js <vinhkute17102004@gmail.com>
2024-09-24 08:46:58 +0700

    create file baitest

---
+++ lesson7/scrips.js
@@ -0,0 +1,8 @@
+let num1 = 13;
+let num2 = 2;
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num1 % num2);
+console.log(num1 ** num2);
